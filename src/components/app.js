7dc9604c1ea6a3886efe3b8f541ef9de753a563d
ssmirnova <svetlana.smirnova213@gmail.com>
2021-01-18 15:01:14 +0100

    Initial commit, app.js structure drafted

---
+++ src/components/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+
+    return (
+        <>
+        <Home/>
+        <CoffeeTypes/>
+        <Menu/>
+        <Giftset/>
+        <Combo/>
+        <Footer/>
+        </>
+    )
+}
+
+export default App;
