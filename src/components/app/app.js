2e38c7d780336b99b2c1e3aeb26ea3b5b06b202d
ssmirnova <svetlana.smirnova213@gmail.com>
2021-01-18 15:53:26 +0100

    Home in progress: pending icon and MenuComponent

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import Home from '../home/home';
 
 const App = () => {
 
     return (
         <>
         <Home/>
-        <CoffeeTypes/>
+        {/* <CoffeeTypes/>
         <Menu/>
         <Giftset/>
         <Combo/>
-        <Footer/>
+        <Footer/> */}
         </>
     )
 }
---
+++ src/components/home/home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './home.css';
+
+const Home = () => {
+
+    return (
+        <div className="container home">
+            <div className="left block">
+                <div className="moto">YOUR <span>PERSONALIZED</span> COFFEE</div>
+            </div>
+
+            <div className="home-image">
+                
+            </div>
+
+            <div className="right block">
+                <div className="navigation">{/* {navigation} */}</div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Home;
