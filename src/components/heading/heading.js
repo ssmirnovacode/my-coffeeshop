e9bbf8a19d6739477e8dc40c2115cb10cc842eec
ssmirnova <svetlana.smirnova213@gmail.com>
2021-01-19 17:20:03 +0100

    Beverages grid made responsive

--- src/components/heading/heading.js
+++ src/components/heading/heading.js
@@ -3,7 +3,7 @@ import './heading.scss';
 
 const Heading = ({small, big}) => {
     return (
-        <div className="heading beverages">
+        <div className="heading">
                 <div className="heading_small">{small}</div>
                 <div className="heading_big">{big}</div>
         </div>
