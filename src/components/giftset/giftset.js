f4b906159bc840bd01fc6503ba964a20e2530c45
ssmirnova <svetlana.smirnova213@gmail.com>
2021-01-19 16:07:24 +0100

    Tabs block in giftbox done, pending combing

--- src/components/giftset/giftset.js
+++ src/components/giftset/giftset.js
@@ -9,14 +9,19 @@ const GiftSet = () => {
             <div className="giftset_container">
                 <div className="giftset_img"></div>
                 <div className="giftset_content">
-                    <div className="giftset_price">15.00 $</div>
-                    <div className="giftset_title">Cool giftset</div>
-                    <div className="giftset_text">Awesome text description. Awesome text description. Awesome text description. 
-                                                    Awesome text description. Awesome text description. Awesome text description. 
-                                                    Awesome text description. Awesome text description. </div>
-                    <button className="giftset_btn">ORDER NOW</button>
-                    <div className="giftset_details"><a href="#">Details</a></div>
-
+                    
+                        <div className="giftset_price">15.00 $</div>
+                        <div className="giftset_title">Cool giftset</div>
+                        <div className="giftset_text">Awesome text description. Awesome text description. Awesome text description. 
+                                                        Awesome text description. Awesome text description. Awesome text description. 
+                                                        Awesome text description. Awesome text description. </div>
+                        <button className="giftset_btn">ORDER NOW</button>
+                        <div className="giftset_details"><a href="#">Details</a></div>
+                </div>
+                <div className="giftset_tabs">
+                    <div className="giftset_tabs_item active">1</div>
+                    <div className="giftset_tabs_item">2</div>
+                    <div className="giftset_tabs_item">3</div>
                 </div>
             </div>
         </section>
