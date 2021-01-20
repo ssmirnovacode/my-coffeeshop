d9498974bc1fba521d822cd564e65b8135e55c4e
ssmirnova <svetlana.smirnova213@gmail.com>
2021-01-20 19:00:03 +0100

    Beverages and BevITems GET server data enabled

--- src/components/beverage-item/beverage-item.js
+++ src/components/beverage-item/beverage-item.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './beverage-item.scss';
 
-const BeverageItem = () => { //as props we will pass {img, title, text}
+const BeverageItem = ({item}) => { 
+    const {title, image, text} = item; 
     return(
         <div className="beverage-item_container">
-            <div className="beverage-item beverage-item_img"><img src="https://png.pngtree.com/png-vector/20190529/ourlarge/pngtree-coffee-cup-icon-png-image_1117239.jpg" alt="coffee icon"/></div>
-            <div className="beverage-item beverage-item_title">Item title</div>
-            <div className="beverage-item beverage-item_text">Some description here... Some description here... Some description here...</div>
+            <div className="beverage-item beverage-item_img"><img src={image} alt={title}/></div>
+            <div className="beverage-item beverage-item_title">{title}</div>
+            <div className="beverage-item beverage-item_text">{text}</div>
         </div>
     )
 }
--- src/components/beverages/beverages.js
+++ src/components/beverages/beverages.js
@@ -1,21 +1,70 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './beverages.scss';
 import BeverageItem from '../beverage-item/beverage-item';
 import Heading from '../heading/heading';
+import {connect} from 'react-redux';
+import { beveragesLoaded, beveragesError, beveragesRequested } from '../../actions/beveragesAC';
+import baseURL from '../../services/baseURL';
+import RequestService from '../../services/requestService';
 
-const Beverages = () => {
-    return (
-        <section>
-            <Heading small={'Your Personalized Coffee'} big={'COFFEE BUILD YOUR BASE'}/>
-            <div className="beverages_container">
-                <BeverageItem/>
-                <BeverageItem/>
-                <BeverageItem/>
-                <BeverageItem/>
+class Beverages extends Component {
 
-            </div>
-        </section>
-    )
+    componentDidMount() {
+        this.props.beveragesRequested();
+
+        const getService = new RequestService();
+
+        getService.getMenuItems(baseURL+'beverages')
+        .then(res => this.props.beveragesLoaded(res))
+        .catch(() => this.props.beveragesError());
+    }
+
+    render() {
+
+        const {beverages, /* loading, error */} = this.props;
+
+        /* if (loading) {
+            return(
+                <Loading/>
+            )
+        }
+
+        else if (error) {
+            return {
+                <Error/>
+            }
+        } */
+
+        return (
+            <section>
+                <Heading small={'Your Personalized Coffee'} big={'COFFEE BUILD YOUR BASE'}/>
+                <div className="beverages_container">
+                    {
+                        beverages.map(item => {
+                            return(
+                                <BeverageItem key={item.id} item={item}/>
+                            )
+                        })
+                    }
+                </div>
+            </section>
+        )
+    }
+    
 }
 
-export default Beverages;
+const mapStateToProps = (state) => {
+    return {
+        beverages: state.beverages,
+        loading: state.loading,
+        error: state.error
+    }
+}
+
+const mapDispatchToProps = {
+    beveragesLoaded,
+    beveragesRequested,
+    beveragesError
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Beverages);
