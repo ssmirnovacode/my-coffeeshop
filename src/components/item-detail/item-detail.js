46257160f803edc3d257bac53252a37cdc7f27ea
ssmirnova <svetlana.smirnova213@gmail.com>
2021-01-21 16:58:39 +0100

    ItemDetail GET gets the correct item but in an array of one

--- src/components/item-detail/item-detail.js
+++ src/components/item-detail/item-detail.js
@@ -2,19 +2,38 @@ import React, {Component} from 'react';
 import './item-detail.scss';
 import {connect} from 'react-redux';
 import {toggleModal} from '../../actions/modal';
+import RequestService from '../../services/requestService';
+import baseURL from '../../services/baseURL';
 
 class ItemDetail extends Component {
 
     render() {
+ 
         if (this.props.modal.shown) {
+
+            const requestService = new RequestService();
+
+            /* let arrClicked = []; */
+
+            requestService.getMenuItems(baseURL+'menuItems')
+            .then(res => res.filter(item => item.id === this.props.modal.itemId))
+            .then(item => {
+                console.log(item) // [{item}]
+
+            })
+            
+            
+           /*  const {title, price, image, content} = arrClicked[0]; */
+
             return(
                 <div className="modal_container">
                     <div className="modal_dialog">
                         <div className="modal_content">
                             <div data-close className="modal_close" onClick={() => this.props.toggleModal()}>&times;</div>
-                            <div className="modal_title">item title</div>
+                            <div className="modal_title">title{/* {item.title} */}</div>
+                            <div className="modal_price">1000{/* {item.price} */}</div>
                             <div className="modal_image"></div>
-                            <div className="modal_text">text text text text text text text text text text text text</div>
+                            <div className="modal_text"> lorem ipsum dolor safaldhlkashdklahsdlashdlashl{/* {item.content} */}</div>
                             <button className="menu-item_btn">Order now</button>
                         </div>
                     </div>
