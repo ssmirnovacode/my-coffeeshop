1c0e409d04fa9f2051664e04b745265826e865b5
ssmirnova <svetlana.smirnova213@gmail.com>
2021-01-21 19:22:04 +0100

    ItemDetail refactored, modal closing at click away activated

--- src/components/item-detail/item-detail.js
+++ src/components/item-detail/item-detail.js
@@ -6,16 +6,18 @@ import {toggleModal} from '../../actions/modal';
 class ItemDetail extends Component {
 
     render() {
+
         if (this.props.modal.shown) {
+            const {title, price, image, content} = this.props.modal.item;
             return(
-                <div className="modal_container">
+                <div className="modal_container"  onClick={() => this.props.toggleModal()}>
                     <div className="modal_dialog">
                         <div className="modal_content">
                             <div data-close className="modal_close" onClick={() => this.props.toggleModal()}>&times;</div>
-                            <div className="modal_title">{/* title */}{this.props.modal.item.title}</div>
-                            <div className="modal_price">{/* 1000 */}{this.props.modal.item.price}</div>
-                            <div className="modal_image"></div>
-                            <div className="modal_text"> {/* lorem ipsum dolor safaldhlkashdklahsdlashdlashl */}{this.props.modal.item.content}</div>
+                            <div className="modal_title">{title}</div>
+                            <div className="modal_price">{price}</div>
+                            <div className="modal_image"><img src={image} alt={title}/></div>
+                            <div className="modal_text">{content}</div>
                             <button className="menu-item_btn">Order now</button>
                         </div>
                     </div>
