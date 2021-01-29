734140096d535c90a90d3f67776edca567cdd4ec
ssmirnova <svetlana.smirnova213@gmail.com>
2021-01-29 19:07:13 +0100

    Navigation toggle enabled

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './navigation.scss';
 
-const Navigation = () => {
-    return(
-        <nav>
-            <div className="nav_burger">            
-                 &#9776;
-            </div>
-            <ul className="nav_menu">
-                <li><a href="#"><span>MY COFFEESHOP</span></a></li>
-                <li><a href="#beverages">OUR BEVERAGES</a></li>
-                <li><a href="#menu">CHOOSE YOUR DRINK</a></li>
-                <li><a href="#giftset">GIFTSET</a></li>
-                <li><a href="#combo">OUR COMBOS</a></li>
-            </ul>
-        </nav>
-    )
+class Navigation extends Component {
+    constructor(props) {
+        super(props);
+        this.toggleMenu = this.toggleMenu.bind(this);
+    }
+
+    toggleMenu = () => {
+        if(document.querySelector('.nav_menu_sidebar').classList.contains('active')) {
+            document.querySelector('.nav_menu_sidebar').classList.remove('active');
+        }
+        else document.querySelector('.nav_menu_sidebar').classList.add('active');
+    }
+
+    render() {
+        return(
+            <nav>
+                <div className="nav_burger" onClick={this.toggleMenu}>            
+                     &#9776;
+                </div>
+                <ul className="nav_menu">
+                    <li><a href="#"><span>MY COFFEESHOP</span></a></li>
+                    <li><a href="#beverages">OUR BEVERAGES</a></li>
+                    <li><a href="#menu">CHOOSE YOUR DRINK</a></li>
+                    <li><a href="#giftset">GIFTSET</a></li>
+                    <li><a href="#combo">OUR COMBOS</a></li>
+                </ul>
+                <ul className="nav_menu_sidebar">
+                    <li><a href="#"><span>MY COFFEESHOP</span></a></li>
+                    <li><a href="#beverages">OUR BEVERAGES</a></li>
+                    <li><a href="#menu">CHOOSE YOUR DRINK</a></li>
+                    <li><a href="#giftset">GIFTSET</a></li>
+                    <li><a href="#combo">OUR COMBOS</a></li>
+                </ul>
+            </nav>
+        )
+    }
+    
 }
 
 export default Navigation;
