07fcd89e55a31c8a866f5d5a8adbfe36bc0971f3
ssmirnova <svetlana.smirnova213@gmail.com>
2021-02-02 17:55:12 +0100

    Cart refactored and styled like a normal component

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 import './cart.scss';
 import {connect} from 'react-redux';
-import {deleteFromCart, toggleCart, plusQty, minusQty} from '../../actions/cartAC';
+import {deleteFromCart, plusQty, minusQty} from '../../actions/cartAC';
 import {toggleOrderForm} from '../../actions/orderAC';
+import {Link} from 'react-router-dom';
+import basePath from '../../services/basePath';
 
 class Cart extends Component {
 
@@ -12,47 +14,37 @@ class Cart extends Component {
             total += item.price * item.qty;
         })
             return(
-                <div className="modal_container" onClick={(event) => {
-                                                        if (event.target.classList.contains('modal_container')) {
-                                                            return this.props.toggleCart(); 
-                                                            }    
-                                                        }}>
-                    <div className="modal_dialog">
-                        <div className="modal_content">
-                            <div className="modal_close" onClick={() => this.props.toggleCart()}>&times;</div> {/* Change to 'continue shopping' */}
-                            <div className="modal_title">Items in your cart:</div>
-                            <div className="modal_cart_list">
-                                {
-                                    this.props.cart.items.map(item => {
-                                        return(
-                                            <div className="modal_cart_item" key={item.id}>
-                                                <div className="modal_cart_item_title">{item.title}</div>
-                                                <div className="modal_cart_item_price">Price: {item.price} $</div>
-                                                <div onClick={() => this.props.deleteFromCart(item.id)} className="modal_cart_item_delete"><i className="fa fa-trash-o"></i></div>
-                                                <div className="modal_cart_item_img"><img src={item.image} alt={item.title}/></div>                                               
-                                                <div className="modal_cart_item_qty">Quantity: 
-                                                    <button className="plus" onClick={() => {
-                                                        this.props.plusQty(item.id);
-                                                        console.log(item);
-                                                        }}> + </button>
-                                                     {item.qty} 
-                                                    <button className="minus" onClick={()=> this.props.minusQty(item.id)}> - </button>
-                                                </div>
-                                                
-                                                
-                                            </div>
-                                        )
-                                    })
-                                }
-                            </div>
-                            <div className="modal_cart_total">Total: {total} $</div>
-                            <button className="modal_btn" onClick={() => {
-                                this.props.toggleOrderForm();
-                                this.props.toggleCart();
-                                }}>Make your order</button>
-                            <div className="modal_cart_continue" onClick={() => this.props.toggleCart()}>Continue shopping</div>
-                        </div>
+                <div className="cart_container" >
+
+                    <div className="cart_title">Items in your cart:</div>
+                    <div className="cart_list">
+                        {
+                            this.props.cart.items.map(item => {
+                                return(
+                                    <div className="cart_item" key={item.id}>
+                                        <div className="cart_item_title">{item.title}</div>
+                                        <div className="cart_item_price">Price: {item.price} $</div>
+                                        <div onClick={() => this.props.deleteFromCart(item.id)} className="modal_cart_item_delete"><i className="fa fa-trash-o"></i></div>
+                                        <div className="cart_item_img"><img src={item.image} alt={item.title}/></div>                                               
+                                        <div className="cart_item_qty">Quantity: 
+                                            <button className="plus" onClick={() => {
+                                                this.props.plusQty(item.id);
+                                                console.log(item);
+                                                }}> + </button>
+                                                {item.qty} 
+                                            <button className="minus" onClick={()=> this.props.minusQty(item.id)}> - </button>
+                                        </div>
+                                        
+                                        
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
+                    <div className="modal_cart_total">Total: {total} $</div>
+                    <button className="modal_btn" onClick={() => this.props.toggleOrderForm()}>Make your order</button>
+                    <div className="modal_cart_continue"><Link to={`${basePath}/menu`}>Continue shopping</Link></div>
+
                 </div>
             )
                
@@ -66,7 +58,6 @@ const mapStateToProps = (state) => {
 }
 
 const mapDispatchToProps = {
-    toggleCart,
     deleteFromCart,
     plusQty,
     minusQty,
