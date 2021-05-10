26c963f6ade4c1ed96459cd15d479cd477f4ac32
ssmirnova <svetlana.smirnova213@gmail.com>
2021-05-10 18:37:10 +0200

    ItemDetail pending fetch

--- src/components/item-detail/item-detail.js
+++ src/components/item-detail/item-detail.js
@@ -1,16 +1,52 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useMemo} from 'react';
 import './item-detail.scss';
 import {connect} from 'react-redux';
 import {addToCart} from '../../redux/actions/cartAC';
 //import toggleButton from '../../local-functions/toggleButton';
 import {Link} from 'react-router-dom';
 import basePath from '../../assets/basePath';
+import firebase from '../../firebase.config';
 
 const ItemDetail = (props) => {
     const {itemId} = props;
-    const allItems = [...props.menuItems, ...props.combos, ...props.giftset.items]; // 2 из 3 массивов не загрузятся, но нужный будет
-    const item = allItems.filter(item => item.id === itemId)[0]; 
-    const {id, image, title, price, content} = item;
+
+    const [localState, setLocalState] = useState({
+        item: null,
+        loading: true,
+        error: false
+    });
+
+    const itemRef = useMemo( () => firebase.database().ref('menuItems'), []);
+
+    useEffect( () => {
+        itemRef.on('value', (snapshot) => {
+            const items = snapshot.val();
+            if (items) {
+                const itemList = [];
+                for (let id in items) {
+                    itemList.push({ id, ...items[id] });
+                };
+
+                setLocalState({
+                    item: itemList.filter(item => item.id === itemId)[0],
+                    loading: false,
+                    error: false
+                });
+                console.log(`Item received from server`);
+            }
+            else {
+                setLocalState((localState) => ({
+                    item: localState.item,
+                    loading: false,
+                    error: true
+                }));
+                console.log(`ERROR in fetching`);
+            }
+        });
+    }, [itemRef, itemId])
+    /* const allItems = [...props.menuItems, ...props.combos, ...props.giftset.items]; // 2 из 3 массивов не загрузятся, но нужный будет
+    const item = allItems.filter(item => item.id === itemId)[0];  */
+    const {id, image, title, price, content} = localState.item;
 
     const [activeBtn, setActiveBtn] = useState('addToCart');
 
@@ -35,7 +71,7 @@ const ItemDetail = (props) => {
                 {
                     activeBtn === 'addToCart' ? <button className="item-detail_btn" data-id={id}
                     onClick={(e) => {                                           
-                        props.addToCart(item);
+                        props.addToCart(localState.item);
                         toggleBtn();
                         }}>ADD TO CART</button> :
                     <Link to={`${basePath}/cart`}  className="item-detail_btn_viewcart" data-id={id}>VIEW CART</Link>
