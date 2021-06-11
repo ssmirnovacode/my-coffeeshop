e7b54aa327e62f1f7809d7f66d38bc32e8a664d9
ssmirnova <svetlana.smirnova213@gmail.com>
2021-06-11 12:09:57 +0200

    Giftset styling reconsidered

--- src/components/giftset-item/giftset-item.js
+++ src/components/giftset-item/giftset-item.js
@@ -26,24 +26,26 @@ const GiftsetItem = ({item, addToCart}) => {
                 </Link>
             </div>
             <div className="giftset_content">
-                
                     <div className="giftset_price">{price} $</div>
                     <div className="giftset_title">{title}</div>
                     <div className="giftset_text">{content}</div>
-                    {
-                        activeBtn === 'addToCart' ? <button className="giftset_btn" data-id={id} onClick={(e) => {
-                            addToCart();
-                            toggleBtn();            
-                        }}>ADD TO CART</button> :
-                        <div className="giftset-item_msg-btn-box">
-                            <Link to={`${basePath}/cart`}  className="giftset_btn_viewcart" data-id={id}>VIEW CART</Link>
-                            {
-                                activeBtn === 'addToCart' ? null : <div className="giftset-item_msg-btn-box_message">Added to cart!</div>
-                            }
-                        </div>
-                    }
-                    <div className="giftset_details"><Link to={`${basePath}/giftset/${id}`}>Details</Link></div><br/>
-                    
+                    <div className="giftset_wrapper">
+                    <div className="giftset_btns">
+                        {
+                            activeBtn === 'addToCart' ? <button className="giftset_btn" data-id={id} onClick={(e) => {
+                                addToCart();
+                                toggleBtn();            
+                            }}>ADD TO CART</button> :
+                            <div className="giftset-item_msg-btn-box">
+                                <Link to={`${basePath}/cart`}  className="giftset_btn_viewcart" data-id={id}>VIEW CART</Link>
+                                {
+                                    activeBtn === 'addToCart' ? null : <div className="giftset-item_msg-btn-box_message">Added to cart!</div>
+                                }
+                            </div>
+                        }
+                        <div className="giftset_details"><Link to={`${basePath}/giftset/${id}`}>Details</Link></div><br/>
+                    </div>
+                    </div>
             </div>
         </div>
     )
