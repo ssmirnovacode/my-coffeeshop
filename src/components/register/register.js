41eeda7808122b0c2d871fc1e53c192ffb88b0a8
ssmirnova <svetlana.smirnova213@gmail.com>
2021-06-16 16:57:05 +0200

    Register component enabled and styled, mixin added to variables

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './register.scss';
+import firebase from '../../firebase.config';
+import {Link} from 'react-router-dom';
+import basePath from '../../assets/basePath';
 
 const Register = props => {
 
+    const [regState, setLoginState] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: '',
+        error: ''
+    });
+
+    const handleChange = e => {
+        setLoginState(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        firebase.auth().createUserWithEmailAndPassword(regState.email, regState.password)
+        .then( () => console.log('registered!'))
+        .catch(err => console.error(err.message));
+    }
+
     return(
-        <div>Register page</div>
+        <section className="reg wrapper">
+            <form className="reg form" onSubmit={handleSubmit}>
+                <h2 className="reg_title">Enter your data to register:</h2>
+                <div className="reg form-field">
+                    <label htmlFor="name">Name: </label>
+                    <input type="text" name="name" placeholder="Enter your name" value={regState.name} onChange={e => handleChange(e)} />
+                </div>
+                <div className="reg form-field">
+                    <label htmlFor="email">Email: </label>
+                    <input type="email" name="email" placeholder="Enter your email" value={regState.email} onChange={e => handleChange(e)} />
+                </div>
+                <div className="reg form-field">
+                    <label htmlFor="password">Password: </label>
+                    <input type="password" name="password" placeholder="Enter your password" value={regState.password} onChange={e => handleChange(e)} /> 
+                </div>
+                <div className="reg form-field repeat">
+                    <label htmlFor="password2">Repeat password: </label>
+                    <input type="password" name="password2" placeholder="Repeat your password" value={regState.password2} onChange={e => handleChange(e)} /> 
+                </div>
+                
+                <input type="submit" className="login_btn" />
+            </form>
+            <div>
+                Already have an account? Sign in <Link to={`${basePath}/login`}>here</Link>.
+            </div>
+        </section>
     )
 }
 
