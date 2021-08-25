8dd760bee383d262ffc3c2b9efc9536079f3aa77
ssmirnova <svetlana.smirnova213@gmail.com>
2021-08-25 16:32:22 +0200

    handler function generalised with an arg fir any items

--- src/redux/sagas/handlers/beveragesHandler.js
+++ src/redux/sagas/handlers/beveragesHandler.js
@@ -2,14 +2,20 @@ import { call, put } from "redux-saga/effects";
 import { getItems, baseApiUrl } from '../../../services/service';
 import { beveragesLoaded, beveragesError, beveragesRequested } from '../../../redux/actions/beveragesAC';
 
-export function* handleGetBeverages(action) {
+export function* handleGetBeverages(items) {
     try {
-        console.log('handler run');
-        const res = yield call(() => getItems(`${baseApiUrl}/beverages`)); //making a request and waiting for it to finish
-        
+        const res = yield call(() => getItems(`${baseApiUrl}/${items}`)); //making a request and waiting for it to finish
         yield put(beveragesLoaded(res)); // dispatching another action after getting data
     }
     catch(err) {
         console.log(err);
+        
     }
+    /* const res = yield call(() => getItems(`${baseApiUrl}/beverages`));
+    if (res.length > 0) {
+        yield put(beveragesLoaded(res));
+    }
+    else {
+        yield put(beveragesError());
+    } */
 }
--- src/redux/sagas/rootSaga.js
+++ src/redux/sagas/rootSaga.js
@@ -4,6 +4,5 @@ import { handleGetBeverages } from './handlers/beveragesHandler';
 // watcher - generator function:
 export function* watcherSaga() {
     //takeLatest means that in case of various requests the last one will be taken and previous will be neglected
-    console.log('watcher run');
-    yield takeLatest('BEVERAGES_REQUESTED', handleGetBeverages);
+    yield takeLatest('BEVERAGES_REQUESTED', () => handleGetBeverages('beverages'));
 }
