485e237648905dd2ce7cebead2c4308dc53087e6
ssmirnova <svetlana.smirnova213@gmail.com>
2021-08-30 13:25:08 +0200

    HomePage economic rendering enabled, BUG in PlaceOrder on order submit

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Beverages from '../beverages/beverages';
 import Menu from '../menu/menu';
 import Giftset from '../giftset/giftset';
@@ -8,10 +8,15 @@ import Combo from '../combo/combo';
 const HomePage = () => {
 
     const dispatch = useDispatch();
+    
+    const state = useSelector(state => state);
 
     useEffect(() => {
-        dispatch({ type: 'FETCH_ALL_DATA' })
-    }, [dispatch])
+        if (!state.beverages.items.length || !state.combos.items.length || !state.giftset.items.length || !state.menuItems.items.length) {
+            dispatch({ type: 'FETCH_ALL_DATA' });
+            console.log('fetchAll dispatched')
+        }
+    }, [])
 
     return (
         <>
